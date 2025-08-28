768644ed131fa5a0cdc86751161d7cf46a7c2088
XuYuanzhe <38235055+XuYuanzhe@users.noreply.github.com>
2025-08-28 09:11:28 +0000

    Deploying to gh-pages from @ XuYuanzhe/XuYuanzhe.github.io@60bcd0fa2436ef29e736ec4e5ed260e2750b0deb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-init-py",
+        },{id: "post-yml-amp-yaml",
+        
+          title: "yml &amp; yaml ?",
+        
+        description: "傻傻分不清楚",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/yaml/";
+          
+        },
+      },{id: "post-init-py",
         
           title: "__init__.py",
         
